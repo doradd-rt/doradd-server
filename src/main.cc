82e4e1c9cbd045296ef50613e7916a453c44d5ed
Marios Kogias <marioskogias@gmail.com>
2024-04-01 11:17:25 +0000

    Add styling

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,6 @@
+#include <cpp/when.h>
 #include <iostream>
 #include <verona.h>
-#include <cpp/when.h>
 
 using namespace verona::rt;
 using namespace verona::cpp;
@@ -8,13 +8,16 @@ using namespace verona::cpp;
 int main(int argc, char **argv) {
   uint8_t nr_cpu = atoi(argv[1]);
 
-	auto& sched = Scheduler::get();
-	Scheduler::set_detect_leaks(true);
-	sched.set_fair(true);
-	sched.init(nr_cpu);
+  auto &sched = Scheduler::get();
+  Scheduler::set_detect_leaks(true);
+  sched.set_fair(true);
+  sched.init(nr_cpu);
 
-  when() << []{ std::cout << "hello behaviours\n"; };
-  when() << []{ while(1); };
+  when() << [] { std::cout << "hello behaviours\n"; };
+  when() << [] {
+    while (1)
+      ;
+  };
 
   sched.run();
 }
