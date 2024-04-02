7e34351871d79084bb0429afae6e4b9fb66be4f6
Marios Kogias <marioskogias@gmail.com>
2024-04-02 11:17:34 +0000

    Run behaviours on eal threads

--- src/main.cc
+++ src/main.cc
@@ -1,23 +1,43 @@
 #include <cpp/when.h>
+#include <dpdk.h>
 #include <iostream>
 #include <verona.h>
 
 using namespace verona::rt;
 using namespace verona::cpp;
 
+struct rte_mempool *pktmbuf_pool;
+
 int main(int argc, char **argv) {
-  uint8_t nr_cpu = atoi(argv[1]);
+
+  printf("Hello dorad DPDK server\n");
+
+  DPDKManager::dpdk_init(&argc, &argv);
+
+  printf("There are %d cores\n", rte_lcore_count());
+
+  // One dispatcher core and the rest are for the runtime
+  uint8_t nr_cpu = rte_lcore_count() - 1;
 
   auto &sched = Scheduler::get();
   Scheduler::set_detect_leaks(true);
   sched.set_fair(true);
   sched.init(nr_cpu);
 
-  when() << [] { std::cout << "hello behaviours\n"; };
   when() << [] {
+    std::cout << "hello behaviours1\n";
+    while (1)
+      ;
+  };
+  when() << [] {
+    std::cout << "hello behaviours2\n";
     while (1)
       ;
   };
 
+  std::cout << "Will now run the scheduluer\n";
   sched.run();
 }
+
+// Temporary here
+void process_pkt(rte_mbuf *pkt) { std::cout << "Will process pkt\n"; }
--- src/verona_external_threading.h
+++ src/verona_external_threading.h
@@ -1,21 +1,51 @@
 #pragma once
+#include <iostream>
+#include <memory>
+
+extern "C" {
+#include <rte_lcore.h>
+}
 
 class PlatformThread {
 public:
+  template <class ThreadArgs> static int proxy_fn(void *args_ptr) {
+    std::cout << "hello from the new thread\n";
+
+    auto args = static_cast<ThreadArgs *>(args_ptr);
+
+    // Deferred, exception-safe destructor.
+    std::unique_ptr<ThreadArgs> thread_args_ptr(args);
+
+    std::apply(std::get<0>(*args), std::get<1>(*args));
+
+    return 0;
+  }
+
   template <typename F, typename... Args>
-  PlatformThread(F &&f, Args &&...args) {}
+  PlatformThread(F &&f, Args &&...args) {
+    std::cout << "Creating a verona thread\n";
+
+    uint8_t lcore_id = 1;
+    auto fused_args = std::forward_as_tuple(args...);
+
+    typedef std::tuple<typename std::decay<F>::type,
+                       std::tuple<typename std::decay<Args>::type...>>
+        ThreadArgs;
+    // Deferred, exception-safe destructor.
+    auto thread_args_ptr =
+        std::make_unique<ThreadArgs>(f, std::move(fused_args));
+    rte_eal_remote_launch(&proxy_fn<ThreadArgs>, thread_args_ptr.get(),
+                          lcore_id);
+  }
 
   void join() { assert(0); }
 };
 
 class Topology {
 public:
-  size_t get(size_t index) {
-    assert(0);
-    return index;
-  }
+  size_t get(size_t index) { return index; }
 
-  static void init(Topology *) noexcept { assert(0); }
+  static void init(Topology *) noexcept {}
 };
 
 namespace cpu {
